4473f3552d1d82040fa5f82211d537c17c4cefc8
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-07-20 19:28:17 +0530

    added googleApiClient module

--- app/src/main/java/com/example/locationapp/MapsActivity.java
+++ app/src/main/java/com/example/locationapp/MapsActivity.java
@@ -2,14 +2,20 @@ package com.example.locationapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
+
 import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,9 +27,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        LocationListener
-        {
+        LocationListener {
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,15 +53,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        double lat=27.215790;
-        double lon = 77.976905;
-        // Added a marker in india and move the camera
-        LatLng india = new LatLng(lat,lon);
-        mMap.addMarker(new MarkerOptions().position(india).title("Your Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(india));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(india,12.0f));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            buildGoogleClintApp();
+            mMap.setMyLocationEnabled(true);
+        }
     }
-
+            protected synchronized void buildGoogleClintApp(){
+            googleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+            googleApiClient.connect();
+            }
 
             @Override
             public void onLocationChanged(@NonNull Location location) {
